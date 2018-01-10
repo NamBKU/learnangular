115ccd94534e807166346a1a94198009f9ecf21e
NamBKU <nguyenngocnambka@gmail.com>
2018-01-10 21:44:51 +0700

    first angular

---
+++ my-app/src/app/word/word.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    templateUrl:'./word.component.html',
+    selector:'app-word'
+})
+
+export class WordComponent{
+    en:string='hello';
+    vn:string='xin chao';
+}
